b4142603008ebdd8084dbfd132ab3a5e72ba5601
Juno144 <pawelekstanley@gmail.com>
2023-10-17 00:52:03 +0200

    Initial commit

---
+++ src/main/java/org/lista1/Main.java
@@ -0,0 +1,10 @@
+package org.lista1;
+
+public class Main {
+   public static void main(final String[] args) {
+      System.out.println("Hello world!");
+   }
+   public static int square(final int input) {
+      return input * input;
+   }
+}
---
+++ src/test/java/org/lista1/AsdfghTest.java
@@ -0,0 +1,13 @@
+package org.lista1;
+
+import org.junit.*;
+
+public class AsdfghTest {
+   @Test
+   public void testowanie(){
+      assert Main.square(2) == 4;
+      assert Main.square(3) == 9;
+      assert Main.square(4) == 16;
+   }
+
+}
