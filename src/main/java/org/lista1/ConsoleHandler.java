cf17049f8838b62a3c74600d720ddd2a28ea75b9
Juno144 <pawelekstanley@gmail.com>
2023-10-18 15:40:47 +0200

    fixes

---
+++ src/main/java/org/lista1/ConsoleHandler.java
@@ -0,0 +1,14 @@
+package org.lista1;
+
+import java.util.Scanner;
+
+public class ConsoleHandler {
+   public static void main(String[] args) {
+      System.out.println("Podaj działanie w notacji polskiej:");
+      Scanner scanner = new Scanner(System.in);
+      while (true) {
+         String string = scanner.nextLine();
+         System.out.println(NotationParser.parse(string).calculate());
+      }
+   }
+}
